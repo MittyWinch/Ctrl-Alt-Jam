d24df1eb38b8b32cea0c6ef324c55ef00b436d39
Ana Célia <79975616+MittyWinch@users.noreply.github.com>
2022-09-06 19:05:19 -0300

    Player Movement, Camera Follow

--- Ctrl Alt Jam/Assets/Scripts/Player_Controller.cs
+++ Ctrl Alt Jam/Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 6f;
+    private float gravity = 9.8f;
+    private float verticalSpeed = 0;
+    public CharacterController characterController;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+        Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+        characterController.Move(speed * Time.deltaTime * move);
+        if (characterController.isGrounded) verticalSpeed = 0;
+        else verticalSpeed -= gravity * Time.deltaTime;
+        Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
     }
 }
