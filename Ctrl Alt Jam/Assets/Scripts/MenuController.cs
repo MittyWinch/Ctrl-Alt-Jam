cd8fc1bba4a6a5468671b86a01a1b1da771c380c
Ana Célia <79975616+MittyWinch@users.noreply.github.com>
2022-09-08 19:58:22 -0300

    Menu Iniciate

---
+++ Ctrl Alt Jam/Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    public string _Cena;
+    public Animator anim;
+    [SerializeField] private TMP_Text volumeTextoValor = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+
+    public void IniciarJogo()
+    {
+        SceneManager.LoadScene(_Cena);
+        anim.SetBool("isIniciate", true);
+    }
+
+    public void AjustarVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextoValor.text = volume.ToString("0%");
+    }
+    public void VolumeSet()
+    {
+        PlayerPrefs.SetFloat("VolumeGeral", AudioListener.volume);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
